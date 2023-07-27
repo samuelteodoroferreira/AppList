4018e032f3fa3c7584345ea8396a16cc9cb9defc
Samuel Teodoro Ferreira <samuelteodoroferreira@gmail.com>
2023-07-27 13:50:27 -0300

    Estruturação da Tela Formulário

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -59,6 +59,8 @@ dependencies {
     implementation("androidx.compose.ui:ui-graphics")
     implementation("androidx.compose.ui:ui-tooling-preview")
     implementation("androidx.compose.material3:material3")
+    implementation("androidx.coordinatorlayout:coordinatorlayout:1.2.0")
+    implementation("androidx.constraintlayout:constraintlayout:2.1.4")
     debugImplementation("androidx.compose.ui:ui-tooling")
     debugImplementation("androidx.compose.ui:ui-test-manifest")
 }
--- app/src/main/java/devandroid/sam/applistacurso/MainActivity.kt
+++ app/src/main/java/devandroid/sam/applistacurso/MainActivity.kt
@@ -44,3 +44,4 @@ fun GreetingPreview() {
         Greeting("Android")
     }
 }
+
